cbfe7e90e0077be5cafc923dfb9bab4f6e78e8ed
Jan-Marten de Boer <github@johmanx.com>
2018-03-03 10:51:10 +0100

    Add unit test
    Test the different scenarios the cloner has to fulfill.

---
+++ tests/ClonerTest.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Copyright MediaCT. All rights reserved.
+ * https://www.mediact.nl
+ */
+namespace ZeroConfig\Cloner\Tests;
+
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use ZeroConfig\Cloner\Cloner;
+
+/**
+ * @coversDefaultClass \ZeroConfig\Cloner\Cloner
+ */
+class ClonerTest extends TestCase
+{
+    /**
+     * @dataProvider cloneProvider
+     *
+     * @param object   $subject
+     * @param callable $modifier
+     * @param string   $expected
+     *
+     * @return void
+     * @covers ::__invoke
+     * @covers ::cloneObject
+     */
+    public function testInvoke(
+        object $subject,
+        callable $modifier,
+        string $expected
+    ): void {
+        $cloner = new Cloner();
+        $clone  = $cloner($subject);
+
+        $this->assertInternalType('object', $clone);
+
+        $modified = $modifier($clone);
+
+        $this->assertEquals(
+            $expected,
+            $this->createSignature($subject, $modified)
+        );
+    }
+
+    /**
+     * Create a signature for the given subject and clone.
+     *
+     * @param object $subject
+     * @param object $clone
+     *
+     * @return string
+     */
+    private function createSignature(object $subject, object $clone): string
+    {
+        return json_encode(
+            [
+                'subject' => $subject,
+                'clone' => $clone
+            ]
+        );
+    }
+
+    /**
+     * @return object[][]|callable[][]|string[][]
+     */
+    public function cloneProvider(): array
+    {
+        return [
+            $this->createFlatObjectArguments(),
+            $this->createNestedObjectArguments(),
+            $this->createSiblingObjectArguments(),
+            $this->createStaticPropertyObjectArguments()
+        ];
+    }
+
+    /**
+     * @return object[]|callable[]|string[]
+     */
+    private function createFlatObjectArguments(): array
+    {
+        return [
+            $this->createObject(
+                [
+                    'foo' => 'bar',
+                    'baz' => 'qux'
+                ]
+            ),
+            function (object $clone) : object {
+                foreach ((array)$clone as $property => $value) {
+                    $clone->{$property} = strtoupper($value);
+                }
+
+                return $clone;
+            },
+            $this->createSignature(
+                $this->createObject(
+                    [
+                        'foo' => 'bar',
+                        'baz' => 'qux'
+                    ]
+                ),
+                $this->createObject(
+                    [
+                        'foo' => 'BAR',
+                        'baz' => 'QUX'
+                    ]
+                )
+            )
+        ];
+    }
+
+    /**
+     * @return object[]|callable[]|string[]
+     */
+    private function createNestedObjectArguments(): array
+    {
+        $object = $this->createObject(
+            [
+                'foo' => [
+                    'bar' => 'baz'
+                ]
+            ]
+        );
+
+        return [
+            $object,
+            function (object $clone) : object {
+                return $clone;
+            },
+            $this->createSignature($object, $object)
+        ];
+    }
+
+    /**
+     * @return object[]|callable[]|string[]
+     */
+    private function createSiblingObjectArguments(): array
+    {
+        $winter                    = new stdClass();
+        $winter->amsterdam         = new stdClass();
+        $winter->holland           = $winter->amsterdam;
+        $winter->amsterdam->offset = '+1';
+
+        return [
+            $winter,
+            function (object $summer) : object {
+                if (isset($summer->holland->offset)) {
+                    $summer->holland->offset = '+2';
+                }
+
+                return $summer;
+            },
+            $this->createSignature(
+                $winter,
+                $this->createObject(
+                    [
+                        'amsterdam' => [
+                            'offset' => '+2'
+                        ],
+                        'holland' => [
+                            'offset' => '+2'
+                        ]
+                    ]
+                )
+            )
+        ];
+    }
+
+    /**
+     * @return object[]|callable[]|string[]
+     */
+    private function createStaticPropertyObjectArguments(): array
+    {
+        $object = new class {
+            /**
+             * @var string[]
+             */
+            public static $data = ['foo', 'bar', 'baz'];
+        };
+
+        return [
+            $object,
+            function (object $clone) : object {
+                return $clone;
+            },
+            $this->createSignature($object, $object)
+        ];
+    }
+
+    /**
+     * Create an object for the given data.
+     *
+     * @param array $data
+     *
+     * @return object
+     */
+    private function createObject(array $data): object
+    {
+        return json_decode(
+            json_encode($data)
+        );
+    }
+}
